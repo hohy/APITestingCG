5ec9444070c4465e198a292f734aa74ae54e873f
Jan Hýbl <jonnyxxx@gmail.com>
2011-10-15 17:31:15 +0200

    Vytvořen NetBeans projekt pro program.

---
+++ APITestingCG/src/cz/cvut/fit/hybljan2/apitestingcg/APITestingCG.java
@@ -0,0 +1,15 @@
+package cz.cvut.fit.hybljan2.apitestingcg;
+
+/**
+ *
+ * @author Jan Hýbl
+ */
+public class APITestingCG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
