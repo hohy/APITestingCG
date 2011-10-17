139ebfe202e68321882da8946f60c459933d9987
Jan Hýbl <jonnyxxx@gmail.com>
2011-10-17 22:44:36 +0200

    Added Enum to TestLib1

---
+++ testlibs/Lib1/TestLib1/src/testlib1/TestEnum.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testlib1;
+
+/**
+ *
+ * @author Jan Hýbl
+ */
+public enum TestEnum {
+    OK, INFO, WARNING, ERROR
+}
