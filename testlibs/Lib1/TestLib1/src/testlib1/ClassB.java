8a68b615480956b8dbdfda98c7bfb502c107d8d8
Jan Hýbl <jonnyxxx@gmail.com>
2011-10-17 11:44:35 +0200

    Added ClassB to testlib1.

---
+++ testlibs/Lib1/TestLib1/src/testlib1/ClassB.java
@@ -0,0 +1,15 @@
+package testlib1;
+
+/**
+ *
+ * @author Jan Hýbl
+ */
+public class ClassB {
+    public Object someMethod(Integer param1, Integer param2) {
+        return null;
+    }
+    
+    public Object someMethod(Double param1, Double param2) {
+        return null;
+    }
+}
