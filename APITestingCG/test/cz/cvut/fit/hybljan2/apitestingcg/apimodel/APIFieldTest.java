c48cbb5c05c42761c1fa497a59b6db11f96e3c76
Jan Hybl <hybljan2@fit.cvut.cz>
2012-03-15 15:44:50 +0100

    Removed unused code.

--- APITestingCG/test/cz/cvut/fit/hybljan2/apitestingcg/apimodel/APIFieldTest.java
+++ APITestingCG/test/cz/cvut/fit/hybljan2/apitestingcg/apimodel/APIFieldTest.java
@@ -4,25 +4,22 @@
  */
 package cz.cvut.fit.hybljan2.apitestingcg.apimodel;
 
-import java.util.List;
-import java.util.LinkedList;
 import cz.cvut.fit.hybljan2.apitestingcg.apimodel.APIModifier.Modifier;
 import cz.cvut.fit.hybljan2.apitestingcg.scanner.SourceScanner;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
- *
  * @author hohy
  */
 public class APIFieldTest {
-    
-    private static APIField[] testInstances = new APIField[5];
-    
+
+    private static APIField[] testInstances = new APIField[6];
+
     public APIFieldTest() {
     }
 
@@ -36,11 +33,11 @@ public class APIFieldTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -50,13 +47,14 @@ public class APIFieldTest {
      */
     @Test
     public void testToString() {
-        System.out.println("toString");        
+        System.out.println("toString");
         String expResults[] = {
-            "protected final double DOUBLE_CONST",
-            "public static final int INT_CONST",
-            "public static final Object OBJECT_CONST",            
-            "public java.io.File file",
-            "protected float floatVar",            
+                "protected final double DOUBLE_CONST",
+                "public static final int INT_CONST",
+                "public static final java.lang.Object OBJECT_CONST",
+                "public java.io.File file",
+                "protected float floatVar",
+                "public java.util.Map<java.lang.String,java.util.List<java.io.File>> xfileListMap"
         };
         for (int i = 0; i < expResults.length; i++) {
             String expResult = expResults[i];
@@ -64,44 +62,44 @@ public class APIFieldTest {
             assertEquals(expResult, result);
         }
     }
-    
-    
+
+
     @Test
     public void testEquals() {
         System.out.println("equals");
         boolean expResult = true;
-        
+
         List<Modifier> modifiersA = new LinkedList<Modifier>();
         List<Modifier> modifiersB = new LinkedList<Modifier>();
-        modifiersA.add(Modifier.PUBLIC);                
+        modifiersA.add(Modifier.PUBLIC);
         modifiersA.add(Modifier.FINAL);
         modifiersA.add(Modifier.STATIC);
         modifiersB.add(Modifier.STATIC);
         modifiersB.add(Modifier.FINAL);
         modifiersB.add(Modifier.PUBLIC);
-        
+
         APIField instanceA = new APIField("java.io.File", "src", modifiersA);
         APIField instanceB = new APIField("java.io.File", "src", modifiersB);
-        
+
         boolean result = instanceA.equals(instanceB);
-        
+
         assertEquals(result, expResult);
     }
-    
+
     @Test
     public void testEquals2() {
         System.out.println("equals");
         boolean expResult = false;
-        
+
         List<Modifier> modifiersA = new LinkedList<Modifier>();
         List<Modifier> modifiersB = new LinkedList<Modifier>();
-        modifiersA.add(Modifier.PUBLIC);        
-        modifiersB.add(Modifier.PROTECTED);        
+        modifiersA.add(Modifier.PUBLIC);
+        modifiersB.add(Modifier.PROTECTED);
         APIField instanceA = new APIField("java.io.File", "src", modifiersA);
         APIField instanceB = new APIField("java.io.File", "src", modifiersB);
-        
+
         boolean result = instanceA.equals(instanceB);
-        
+
         assertEquals(result, expResult);
-    }    
+    }
 }
